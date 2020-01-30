15c954f6e0d04591cd9df750f5d3d2096439f425
Sumathi Gonugunta <sumathigonugunta16@gmail.com>
2020-01-30 15:12:34 +0530

    Numpy

---
+++ mymodule/ml.py
@@ -0,0 +1,9 @@
+def avg(a):
+    return sum(a)/len(a)
+def empty_list():
+    return []
+def list_zeros(n):
+    l=[0 for i in range(n)]
+    return l
+
+    
---
+++ mymodule/myfunc.py
@@ -0,0 +1,6 @@
+name ='str functions'
+
+def stradd(a,b):
+    return a+b
+def strrep(a,n):
+    return a*n
